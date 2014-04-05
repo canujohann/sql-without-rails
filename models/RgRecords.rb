feec8d98b34226889eaadabdc2110f39c169e15f
canu johann <canu.johannl@gmail.com>
2014-04-05 16:20:58 +0900

    add a simple singleton pattern

--- models/RgRecords.rb
+++ models/RgRecords.rb
@@ -12,7 +12,7 @@ class RgRecords
 
 		# MySQL接続 
 		require_relative("connection.rb")
-		@database, @my = Connection::connect
+		@database, @my = Connection.instance.connect
 
 		#@database = YAML.load_file('config/database.yml')
 		#@my  = Mysql2::Client.new(:host => @database['hostname'].to_s, :username => @database['username'].to_s, :password => @database['password'].to_s, :database => @database['dbname'].to_s)
--- models/connection.rb
+++ models/connection.rb
@@ -1,13 +1,18 @@
 
 require 'xmlsimple'
 require 'mysql2'
+require 'singleton'
 require 'yaml'
 
 class Connection
 
-	def self.connect
+    include Singleton
+	
+	def connect
+
 		database = YAML.load_file('config/database.yml')
 		my  = Mysql2::Client.new(:host => database['hostname'].to_s, :username => database['username'].to_s, :password => database['password'].to_s, :database => database['dbname'].to_s)
+		
 		return database, my
 	end
 end
